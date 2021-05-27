34fc8c188158560316b2340eda084bff8109a0d9
Evan Jensen <evanj545@hotmail.com>
2021-05-27 12:45:38 -0700

    deleted pipfile

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,5 +7,20 @@ def hello_world():
 @app.route("/home")
 def home():
     return "<h1> HELLO <h1>"
+
+
+
+@app.route("/view", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Do Something':
+            print ("view all students")
+        elif request.form['submit_button'] == 'Do Something Else':
+            print ("view specific student")
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('login.html', form=form)
+
 if __name__ == '__main__':
     app.run()
