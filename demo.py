815e9a1f27f2b0cb894d3136668987005db6b206
ejcanoy <euan.james.canoy@gmail.com>
2021-05-29 22:12:45 -0700

    view all students

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import psycopg2
+from flask import Flask, redirect, url_for, render_template, request
 
 #connect to the local host db
 con = psycopg2.connect (
@@ -12,12 +13,12 @@ password = "2fD9vPoMU6HAfMM"
 cur = con.cursor()
 
 #execute query
-cur.execute('SELECT * FROM Administrator')
 cur.execute('Select * from Student')
 rows = cur.fetchall()
 
 for r in rows:
    print(f"ID {r[0]} name {r[1]}")
+
 
 # close cursor
 cur.close()
@@ -25,3 +26,27 @@ cur.close()
 #close the connection
 con.close()
 
+app = Flask(__name__)
+
+@app.route("/") 
+def home():
+    return render_template("studentPage.html", things=rows)
+
+# @app.route("/test")
+# def test():
+#     return render_template("new.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr =user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr} </h1>"
+    
+if __name__ == "__main__":
+     app.run(debug =True)
