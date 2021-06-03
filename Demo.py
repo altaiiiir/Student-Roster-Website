ba2d9ca0156c58a616513433c636d60719158ca5
William <William@WILLIAMS-PC>
2021-06-02 17:49:23 -0700

    Saving progress.

--- Demo.py
+++ Demo.py
@@ -19,6 +19,35 @@ rows = cur.fetchall()
 for r in rows:
     print(f"ID {r[0]} name {r[1]}")
 
+# execute query
+cur.execute('Select * from Course_Catalog')
+courseRows = cur.fetchall()
+
+for r in courseRows:
+    print(f"SLN {r[0]} Name {r[1]} CourseCredits {r[2]} Type {r[3]}")
+
+# execute query
+cur.execute(
+    'Select * from Course_Info '
+    '   JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN)')
+courseInfoRows = cur.fetchall()
+
+# execute query
+cur.execute(
+    'SELECT * FROM Course_Info JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN)'
+    '   JOIN Transcript ON (Course_Info.ID = Transcript.ClassID) '
+    '   JOIN Student ON (Student.ID = Transcript.StudentID) '
+    'WHERE Course_Info.SLN = 10000 AND Course_Info.RoomID = 5')
+courseAttendeesRows = cur.fetchall()
+
+#execute query
+cur.execute('Select * from Course_Info JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN)')
+courseInfoRows = cur.fetchall()
+
+#execute query
+cur.execute('SELECT * FROM Course_Info JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN) JOIN Transcript ON (Course_Info.ID = Transcript.ClassID) JOIN Student ON (Student.ID = Transcript.StudentID) WHERE Course_Info.SLN = 10000 AND Course_Info.RoomID = 5')
+courseAttendeesRows = cur.fetchall()
+
 # close cursor
 cur.close()
 
@@ -33,10 +62,6 @@ def home():
     return render_template("Home.html", things=rows)
 
 
-# @app.route("/test")
-# def test():
-#     return render_template("new.html")
-
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -46,14 +71,17 @@ def login():
         return render_template("Login.html")
 
 
-@app.route("/course-info", methods=["POST", "GET"])
-def courses_info():
+@app.route("/course-catalog", methods=["POST", "GET"])
+def courses_catalog():
     if request.method == "POST":
         user = request.form["nm"]
         return redirect(url_for("user", usr=user))
     else:
-        return render_template("CourseInfo.html")
+        return render_template("CourseCatalog.html", things=courseRows)
 
+@app.route("/course-info")
+def courseInfo():
+    return render_template("courseInfo.html", things = courseInfoRows)
 
 @app.route("/student-page", methods=["POST", "GET"])
 def student_page():
