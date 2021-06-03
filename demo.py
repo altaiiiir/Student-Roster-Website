b5ae785488e32df084c117e7f697b90aa5da2c5a
Evan Jensen <evanj545@hotmail.com>
2021-06-03 00:47:17 -0700

    login

--- demo.py
+++ demo.py
@@ -28,7 +28,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
 
 @app.route("/studentPage", methods =['GET', 'POST'])
 def students():
