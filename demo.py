4861cb5b8c1015ca9315223067cb86d411f14f9c
anabil14 <anabil14@uw.edu>
2021-06-04 16:09:48 -0700

    addremove finished

--- demo.py
+++ demo.py
@@ -12,29 +12,15 @@ password = "2fD9vPoMU6HAfMM"
 #cursor
 cur = con.cursor()
 
-#execute query
-cur.execute('Select * from Student')
-rows = cur.fetchall()
-
-for r in rows:
-   print(f"ID {r[0]} name {r[1]}")
-
-
-# close cursor
-cur.close()
-
-#close the connection
-con.close()
-
 app = Flask(__name__)
+
 
 @app.route("/") 
 def home():
+    cur.execute('Select * from Student')
+    rows = cur.fetchall()
     return render_template("studentPage.html", things=rows)
 
-# @app.route("/test")
-# def test():
-#     return render_template("new.html")
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -44,9 +30,47 @@ def login():
     else:
         return render_template("login.html")
 
+
+
+@app.route("/addRemoveStudent", methods = ["POST","GET"]) 
+def addRemoveStudent():
+    if request.method == "POST":
+        if 'add' in request.form:
+            #need to double check data, protect against sql injections
+            StuID = request.form["studentID"]
+            Fname = request.form["first"]
+            Lname = request.form["last"]
+            gender = request.form["gender"]
+            super = request.form["super"]
+            alias = request.form["alias"]
+            dob = request.form["dob"]
+
+            cur.execute('INSERT INTO Student (StudentID, FirstName, LastName, Alias, \
+                Gender, SuperPower, DOB, IsCurrentlyEnrolled,adminID) \
+                Values(%s,%s,%s,%s,%s,%s,%s,TRUE,1)',(int(StuID),Fname,Lname,alias,gender,super,dob))
+            con.commit()
+            return redirect(url_for("home"))
+        else:
+            #need to double check data, protect against sql injections
+            studID = int(request.form["studID"])
+            # studID = request.form["studID"]
+            cur.execute('DELETE FROM Transcript WHERE Transcript.StudentID = %s',[studID])
+            cur.execute('DELETE FROM Student_Notes WHERE Student_Notes.StudentID = %s',[studID])
+            cur.execute('DELETE FROM STUDENT WHERE ID = %s',[studID])
+            con.commit()
+            return redirect(url_for("home"))
+    else:
+        return render_template("addRemoveStudent.html")
+
 @app.route("/<usr>")
 def user(usr):
     return f"<h1>{usr} </h1>"
-    
+
 if __name__ == "__main__":
      app.run(debug =True)
+
+# close cursor
+cur.close()
+
+#close the connection
+con.close()
