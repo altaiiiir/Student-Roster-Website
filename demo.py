6df2e55a62e53c42e5359ebadf86b4e8a53400b0
Mohamed <faedq@yahoo.com>
2021-06-04 20:56:58 -0700

    Add Course Reengineered

--- demo.py
+++ demo.py
@@ -26,12 +26,13 @@ courseRows = cur.fetchall()
 for r in courseRows:
    print(f"SLN {r[0]} Name {r[1]} CourseCredits {r[2]} Type {r[3]}")
 
+print(id) 
 #execute query
-cur.execute('Select * from Course_Info JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN)')
+cur.execute('Select * from Course_Info JOIN Course_Catalog ON (Course_Info.CourseID = Course_Catalog.ID)')
 courseInfoRows = cur.fetchall()
 
 #execute query
-cur.execute('SELECT * FROM Course_Info JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN) JOIN Transcript ON (Course_Info.ID = Transcript.ClassID) JOIN Student ON (Student.ID = Transcript.StudentID) WHERE Course_Info.SLN = 10000 AND Course_Info.RoomID = 5')
+cur.execute('SELECT * FROM Course_Info JOIN Course_Catalog ON (Course_Info.CourseID = Course_Catalog.ID) JOIN Transcript ON (Course_Info.ID = Transcript.ClassID) JOIN Student ON (Student.ID = Transcript.StudentID) WHERE Course_Info.SLN = 10000 AND Course_Info.RoomID = 5')
 courseAttendeesRows = cur.fetchall()
 
 # close cursor
@@ -55,9 +56,6 @@ def login():
     else:
         return render_template("login.html")
 
-
-
-
 @app.route("/courseInfo") 
 def courseInfo():
     return render_template("courseInfoPage.html", things = courseInfoRows)
@@ -69,30 +67,68 @@ def courseAttendees():
 @app.route("/addCourse", methods = ["POST", "GET"])
 def add():
     if request.method == "POST":
-        sln = request.form["sln"]
-        section = request.form["section"]
-        roomid = request.form["roomid"]
-        instructor = request.form["ins"]
-        time = request.form["time"]
-        quarter = request.form["quarter"]
-        year = request.form["yr"]
+
+        # request data
         name = request.form["nm"]
         cc = request.form["creds"]
         type = request.form["type"]
-        cur.execute("""INSERT INTO Course_Catalog (SLN, Name, CourseCredits, Type) 
-                        VALUES (%s, %s, %s, %s)""", (sln, name, cc, type))
-        cur.execute("""INSERT INTO Course_Info (SLN, Section, RoomID, InstructorName, Time, Quarter, Year) 
-                        VALUES (%s, %s, %s, %s, %s, %s, %s)""", (sln, section, roomid, instructor, time, quarter, year))
-        con.commit()
-        # update table
-        cur.execute('Select * from Course_Catalog')
-        courseRows = cur.fetchall()
-        
-        # update table
-        cur.execute('Select * from Course_Info JOIN Course_Catalog ON (Course_Info.SLN = Course_Catalog.SLN)')
-        courseInfoRows = cur.fetchall()
-        
-        return render_template("courseInfoPage.html", things = courseInfoRows)
+
+        # check if its an add or remove
+        if 'add' in request.form:
+            # adds to course catalog
+            cur.execute("""INSERT INTO Course_Catalog (Name, CourseCredits, Type) 
+                            VALUES (%s, %s, %s)""", (name, cc, type))
+
+            # update table
+            cur.execute('Select * from Course_Catalog')
+            courseRows = cur.fetchall()
+            con.commit()
+
+            return render_template("courseInfoPage.html", things = courseRows)
+        else:
+            # removes course from course catalog
+            cur.execute("""DELETE FROM Course_Catalog WHERE Name = %s""", [name])
+            con.commit()
+
+            # update table
+            cur.execute('Select * from Course_Catalog')
+            courseRows = cur.fetchall()
+            
+            return render_template("coursePage.html", things = courseRows)
+    else:
+        return render_template("addCourse.html")
+
+@app.route("/addClass", methods = ["POST", "GET"])
+def add():
+    if request.method == "POST":
+
+        # request data
+        name = request.form["nm"]
+        cc = request.form["creds"]
+        type = request.form["type"]
+
+        # check if its an add or remove
+        if 'add' in request.form:
+            # adds to course catalog
+            cur.execute("""INSERT INTO Course_Catalog (Name, CourseCredits, Type) 
+                            VALUES (%s, %s, %s)""", (name, cc, type))
+
+            # update table
+            cur.execute('Select * from Course_Catalog')
+            courseRows = cur.fetchall()
+            con.commit()
+
+            return render_template("courseInfoPage.html", things = courseRows)
+        else:
+            # removes course from course catalog
+            cur.execute("""DELETE FROM Course_Catalog WHERE Name = %s""", [name])
+            con.commit()
+
+            # update table
+            cur.execute('Select * from Course_Catalog')
+            courseRows = cur.fetchall()
+            
+            return render_template("coursePage.html", things = courseRows)
     else:
         return render_template("addCourse.html")
 
@@ -103,8 +139,6 @@ def course():
 
 if __name__ == "__main__":
      app.run(debug =True)
-     
-     
      
 # close cursor
 cur.close()
