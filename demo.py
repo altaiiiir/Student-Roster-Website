c2a3c671fb2c6671f84f483845e386b145daeda8
ejcanoy <euan.james.canoy@gmail.com>
2021-06-04 21:52:28 -0700

    Cleaned up code

--- demo.py
+++ demo.py
@@ -27,22 +27,6 @@ app.permanent_session_lifetime = timedelta(minutes=5)
 @app.route("/") 
 def home():
     return render_template("index.html")
-    
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user", usr=user))
-    else:
-        return render_template("login.html")
-
-@app.route("/user")
-def user():
-    if "user" in session:
-        user = session["user"]
-        return f"<h1>{user} </h1>"
-    else:
-        return redirect(url_for("login"))
 
 @app.route("/TranscriptAddRemove", methods=["POST", "GET"])
 def TranscriptAddRemove():
@@ -148,10 +132,6 @@ def addRemoveStudent():
             return redirect(url_for("home"))
     else:
         return render_template("addRemoveStudent.html")
-
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr} </h1>"
 
 if __name__ == "__main__":
      app.run(debug =True)
