efbd02d2746e5201f07e1b7f3420af30adee0198
anabil14 <anabil14@uw.edu>
2021-06-06 21:22:10 -0700

    progress

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
-import psycopg2
+import psycopg2, datetime
 from flask import Flask, redirect, url_for, render_template, request, flash
 
 #connect to the local host db
@@ -14,6 +14,38 @@ cur = con.cursor()
 
 app = Flask(__name__)
 
+def StringChecker(str,name):
+    for x in str:
+        if (ord(x) >= 65 or ord(x) <= 95) and \
+            (ord(x) >= 97 or ord(x) <= 122):
+             continue
+        else:
+            flash(name + " should contain letters only")
+
+def genderChecker(str):
+    if str.isSpace() or str == "":
+        flash("Cannor be empty")
+    elif str.upper() != 'F' or str.upper() != 'M':
+        flash("Unacceptable Gender")
+
+def dobChecker(str):
+    try: 
+        dateObject = datetime.strptime(str,'%d/%m/%y')
+        DOB = str.split("/")
+    except:
+        flash("Invalid DOB")
+
+def StudChecker(str):
+    cur.execute("SELECT EXISTS (SELECT STUDENTID FROM STUDENT WHERE STUDENTID = %s)",(str))
+    exists= fetchall()
+    if exists:
+        flash("Student already exists")
+
+def notStudChecker(str):
+    cur.execute("SELECT EXISTS (SELECT STUDENTID FROM STUDENT WHERE STUDENTID = %s)",(str))
+    exists= fetchall()
+    if not exists:
+        flash("Student doesn't exist")
 
 @app.route("/") 
 def home():
@@ -36,50 +68,120 @@ def login():
 def addRemoveStudent():
     if request.method == "POST":
         if 'add' in request.form:
-            #TODO double check data, protect against sql injections
-            #TODO double check if values are legit
             StuID = request.form["studentID"]
+            StudChecker(StuID)
+
             Fname = request.form["first"]
+            StringChecker(Fname,"First name")
+            
             Lname = request.form["last"]
+            StringChecker(Lname,"Last name")
+           
             gender = request.form["gender"]
+            genderChecker(gender)
+            
             super = request.form["super"]
+            StringChecker(super,"Super abilities")
+            
             alias = request.form["alias"]
+            StringChecker(alias,"Alias")
+            
             dob = request.form["dob"]
+            dobChecker(dob)
 
             cur.execute('INSERT INTO Student (StudentID, FirstName, LastName, Alias, \
                 Gender, SuperPower, DOB, IsCurrentlyEnrolled,adminID) \
                 Values(%s,%s,%s,%s,%s,%s,%s,TRUE,1)',(int(StuID),Fname,Lname,alias,gender,super,dob))
             con.commit()
             return redirect(url_for("home"))
-        elif 'Remove' in request.form:
-            try:
-                studID = int(request.form["studID"])
-                cur.execute("SELECT ID FROM STUDENT WHERE STUDENTID = %s",(studID))
-                studID = cur.fetchall()
-                print("message = " + studID)
-                # cur.execute('DELETE FROM Transcript WHERE Transcript.StudentID = %s',[studID])
-                # # Find the noteID based on studentID, use that to delete notes
-                # # cur.execute('DELETE FROM Notes WHERE Student_Notes.StudentID = %s',[studID])
-                # cur.execute('DELETE FROM Student_Notes WHERE Student_Notes.StudentID = %s',[studID])
-                # cur.execute('DELETE FROM STUDENT WHERE ID = %s',[studID])
-                return redirect(url_for("home"))
-            except:
-                flash('Invalid password provided', 'error')
+        elif 'remove' in request.form:
+
+            studID = int(request.form["studID"])
+            notStudChecker(studID)
+          
+            # cur.execute("SELECT ID FROM STUDENT WHERE STUDENTID = %s",[studID])
+            # studID = cur.fetchall()
+            
+            # cur.execute('DELETE FROM Transcript WHERE Transcript.StudentID = %s',[studID])
+           
+            # # Find the noteID based on studentID, use that to delete notes
+            # cur.execute('SELECT NOTEID FROM Student_Notes WHERE Student_Notes.StudentID = %s',[studID])
+            # noteNumb = cur.fetchall()
+            # cur.execute('DELETE FROM Note WHERE ID = %s',[noteNumb])
+            
+            # cur.execute('DELETE FROM Student_Notes WHERE Student_Notes.StudentID = %s',[studID])
+            # cur.execute('DELETE FROM STUDENT WHERE ID = %s',[studID])
+            return redirect(url_for("home"))
         else:
             StuID = request.form["studentID"]
+            notStudChecker(StuID)
+
             Fname = request.form["first"]
+            StringChecker(Fname,"First name")
+
             Lname = request.form["last"]
+            StringChecker(Lname,"Last name")
+
             gender = request.form["gender"]
+            genderChecker(gender)
+            
             super = request.form["super"]
+            StringChecker(super,"Super abilities")
+            
             alias = request.form["alias"]
+            StringChecker(alias,"Alias")
+            
             dob = request.form["dob"]
+            dobChecker(dob)
+
             enr = request.form["enrollment"]
+            if enr.upper() != "T" or enr.upper() != "F":
+                flash("Must be T or F")
+            if (StuID == ""):
+                flash("Enter studentID", "error")
+                render_template("addRemoveStudent.html")
+
+            studentQuery = """select * from Student where student.ID = %s """ 
+            cur.execute(studentQuery, (StuID,))
+            getStudentQuery = cur.fetchall()
+
+            if Fname == "":
+                qFirst = getStudentQuery[0][0]
+            else:
+                qFirst = Fname
+            
+            if Lname == "":
+                qLast = getStudentQuery[0][1]
+            else:
+                qLast = Fname
+            if gender == "":
+                qGender = getStudentQuery[0][2] 
+            else:
+                qGender = gender;
+            if super == "":
+                qSuper = getStudentQuery[0][3]
+            else:
+                qSuper = super
+            if alias == "":
+                qAlias = getStudentQuery[0][4]
+            else:
+                qAlias = alias
+            if dob == "":
+                qDOB = getStudentQuery[0][5]
+            else:
+                qDOB = dob
+            if enr == "":
+                qENR = getStudentQuery[0][6]
+            else:
+                qENR = enr
+
 
             cur.execute('Update Student \
                 SET FirstName= %s, LastName= %s, Alias= %s, \
                 Gender= %s, SuperPower= %s, DOB= %s, \
-                IsCurrentlyEnrolled= %s WHERE StudentID = %s' ,(Fname,Lname,alias,gender,super,dob,enr,int(StuID)))
+                IsCurrentlyEnrolled= %s WHERE StudentID = %s' ,(qFirst,qLast,qAlias,qGender,qSuper,qDOB,qENR,int(StuID)))
             con.commit()
+            return redirect(url_for("home"))
                
     else:
         return render_template("addRemoveStudent.html")
