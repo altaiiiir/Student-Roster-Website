108e80e74461abf3ca45b3c4e5bc379fbbbd680a
anabil14 <anabil14@uw.edu>
2021-06-06 18:00:43 -0700

    progress

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, flash
 
 #connect to the local host db
 con = psycopg2.connect (
@@ -51,17 +51,36 @@ def addRemoveStudent():
                 Values(%s,%s,%s,%s,%s,%s,%s,TRUE,1)',(int(StuID),Fname,Lname,alias,gender,super,dob))
             con.commit()
             return redirect(url_for("home"))
+        elif 'delete' in request.form:
+            try:
+                # studID = int(request.form["studID"])
+                # cur.execute("SELECT ID FROM STUDENT WHERE STUDENTID = %s",(studID))
+                # studID = cur.fetchall()
+                # print("message = " + studID)
+                # cur.execute('DELETE FROM Transcript WHERE Transcript.StudentID = %s',[studID])
+                # # Find the noteID based on studentID, use that to delete notes
+                # # cur.execute('DELETE FROM Notes WHERE Student_Notes.StudentID = %s',[studID])
+                # cur.execute('DELETE FROM Student_Notes WHERE Student_Notes.StudentID = %s',[studID])
+                # cur.execute('DELETE FROM STUDENT WHERE ID = %s',[studID])
+                return redirect(url_for("home"))
+            except:
+                flash('Invalid password provided', 'error')
         else:
-            #TODO to double check data, protect against sql injections
-            #TODO double check if value is legit
-            studID = int(request.form["studID"])
-            cur.execute('DELETE FROM Transcript WHERE Transcript.StudentID = %s',[studID])
-            # Find the noteID based on studentID, use that to delete notes
-            # cur.execute('DELETE FROM Notes WHERE Student_Notes.StudentID = %s',[studID])
-            cur.execute('DELETE FROM Student_Notes WHERE Student_Notes.StudentID = %s',[studID])
-            cur.execute('DELETE FROM STUDENT WHERE ID = %s',[studID])
+            StuID = request.form["studentID"]
+            Fname = request.form["first"]
+            Lname = request.form["last"]
+            gender = request.form["gender"]
+            super = request.form["super"]
+            alias = request.form["alias"]
+            dob = request.form["dob"]
+            enr = request.form["enrollment"]
+
+            cur.execute('Update Student \
+                SET StudentID = %s, FirstName= %s, LastName= %s, Alias= %s, \
+                Gender= %s, SuperPower= %s, DOB= %s, \
+                IsCurrentlyEnrolled= %s, adminID = 1',(int(StuID),Fname,Lname,alias,gender,super,dob,enr))
             con.commit()
-            return redirect(url_for("home"))
+               
     else:
         return render_template("addRemoveStudent.html")
 
@@ -70,7 +89,8 @@ def user(usr):
     return f"<h1>{usr} </h1>"
 
 if __name__ == "__main__":
-     app.run(debug =True)
+    app.run(debug =True)
+
 
 # close cursor
 cur.close()
